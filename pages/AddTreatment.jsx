6e8365737fb3c3069bac44e4ad28841aa048e7e1
IsuruX98 <isurusanka98@gmail.com>
2023-10-18 15:39:37 +0530

    treatment crud complete

--- pages/AddTreatment.jsx
+++ pages/AddTreatment.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
     View,
     Text,
@@ -8,52 +8,57 @@ import {
     Keyboard,
     TouchableWithoutFeedback,
     ActivityIndicator,
+    Alert,
+    Image
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
-import AxiosApi from '../apis/axios';
+import AxiosAPI from '../apis/axios'
 
 const AddTreatment = () => {
     const [treatmentInfo, setTreatmentInfo] = useState({
         title: '',
         type: '',
         description: '',
-        photo: null,
-        photoUrl: '',
     });
     const [loading, setLoading] = useState(false);
+    const [photo, setPhoto] = useState(null);
+    const [selectedImageUri, setSelectedImageUri] = useState(null);
 
     useEffect(() => {
         (async () => {
-            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
             if (status !== 'granted') {
-                console.log('Sorry, we need camera roll permissions to make this work!');
+                Alert.alert(
+                    'Permission Required',
+                    'Sorry, we need camera roll permissions to make this work!',
+                    [{text: 'OK', onPress: () => console.log('OK Pressed')}]
+                );
             }
         })();
     }, []);
 
     const handleInputChange = (name, value) => {
-        setTreatmentInfo({ ...treatmentInfo, [name]: value });
+        setTreatmentInfo({...treatmentInfo, [name]: value});
     };
 
     const handleImageUpload = async () => {
         let result = await ImagePicker.launchImageLibraryAsync();
 
-        if (!result.cancelled) {
+        if (!result.canceled) {
             const selectedImageUri = result.assets[0].uri;
-            setTreatmentInfo({ ...treatmentInfo, photo: selectedImageUri });
+            setSelectedImageUri(selectedImageUri);
+            setPhoto(selectedImageUri);
         }
     };
-
-    console.log(treatmentInfo.photo)
 
     const handleSubmit = async () => {
         setLoading(true);
 
-        if (treatmentInfo.photo) {
+        if (photo) {
             const formData = new FormData();
             formData.append('file', {
-                uri: treatmentInfo.photo,
+                uri: photo,
                 type: 'image/jpeg',
                 name: 'photo.jpg',
             });
@@ -65,44 +70,77 @@ const AddTreatment = () => {
                     formData
                 );
                 const imageUrl = cloudinaryResponse.data.secure_url;
-                console.log('Image URL:', imageUrl);
 
                 if (cloudinaryResponse.data && cloudinaryResponse.data.secure_url) {
-                    setTreatmentInfo({ ...treatmentInfo, photoUrl: cloudinaryResponse.data.secure_url });
+
+                    const updatedTreatmentInfo = {
+                        ...treatmentInfo,
+                        photoUrl: cloudinaryResponse.data.secure_url,
+                    };
+
+                    if (updatedTreatmentInfo) {
+                        try {
+                            const response = await AxiosAPI.post('treatments', updatedTreatmentInfo);
+                            console.log('Backend response:', response.data);
+                            setTreatmentInfo({
+                                title: '',
+                                type: '',
+                                description: '',
+                                photoUrl: '',
+                            });
+                            setLoading(false);
+                            Alert.alert(
+                                'Success',
+                                'Treatment added successfully.',
+                                [{text: 'OK', onPress: () => console.log('OK Pressed')}]
+                            );
+                        } catch (error) {
+                            console.log('Error adding treatment:', error);
+                            setLoading(false);
+                            Alert.alert(
+                                'Error',
+                                'There was an error adding the treatment. Please try again later.',
+                                [{text: 'OK', onPress: () => console.log('OK Pressed')}]
+                            );
+                        }
+                    }
+
+
                 } else {
                     console.log('Image upload failed.');
                 }
             } catch (error) {
                 console.log('Error uploading image:', error);
+                Alert.alert(
+                    'Image Upload Failed',
+                    'There was an error uploading your image. Please try again later.',
+                    [{text: 'OK', onPress: () => console.log('OK Pressed')}]
+                );
             }
         }
 
-        try {
-            const response = await AxiosApi.post('treatments', treatmentInfo);
-            console.log('Backend response:', response.data);
-            setTreatmentInfo({
-                title: '',
-                type: '',
-                description: '',
-                photo: null,
-                photoUrl: 'default_image.jpg',
-            });
-            console.log('Treatment added successfully.');
-        } catch (error) {
-            console.log('Error adding treatment:', error);
-        } finally {
-            setLoading(false);
-        }
+
     };
 
     const dismissKeyboard = () => {
         Keyboard.dismiss();
     };
 
+    if (loading) {
+        return (
+            <View style={styles.loaderContainer}>
+                <ActivityIndicator size="large" color="#007BFF"/>
+            </View>
+        );
+    }
+
     return (
         <TouchableWithoutFeedback onPress={dismissKeyboard}>
             <View style={styles.container}>
                 <Text style={styles.headerText}>Add Treatments</Text>
+                {selectedImageUri && (
+                    <Image source={{uri: selectedImageUri}} style={styles.selectedImage}/>
+                )}
                 <TextInput
                     style={styles.input}
                     placeholder="Title"
@@ -128,7 +166,6 @@ const AddTreatment = () => {
                 <TouchableOpacity style={styles.button} onPress={handleSubmit}>
                     <Text style={styles.buttonText}>Add Treatment</Text>
                 </TouchableOpacity>
-                {loading && <ActivityIndicator size="large" color="#004AAD" />}
             </View>
         </TouchableWithoutFeedback>
     );
@@ -158,6 +195,12 @@ const styles = StyleSheet.create({
         marginBottom: 16,
         backgroundColor: '#fff',
     },
+    selectedImage: {
+        width: 200, // Set the width as per your UI design
+        height: 200, // Set the height as per your UI design
+        resizeMode: 'contain', // You can adjust the resizeMode property as needed
+        marginBottom: 16,
+    },
     inputMulti: {
         width: '100%',
         height: 70,
@@ -181,6 +224,11 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign: 'center',
         fontWeight: 'bold',
+    },
+    loaderContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
     },
 });
 
